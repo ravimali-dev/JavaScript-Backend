25364c40dffd22f8388b05563848b1b2e483a8cc
Ravi Mali <ravimalakar091@gmail.com>
2026-07-10 15:37:44 +0530

    add asyncHandler utility for wrapping async route handlers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import connectDB from "./db/index.js";
 // import { DB_NAME } from "./constants.js"
 
 connectDB()
+ .then(() => {
+    app.on("error", (error) => {
+      console.log("Server error:", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MongoDB connection failed!", err);
+  });
+
 
 
 
