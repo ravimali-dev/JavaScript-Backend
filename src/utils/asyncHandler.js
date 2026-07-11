01e7c65c85bc3c2ab040d7d35ade38772f870796
Ravi Mali <ravimalakar091@gmail.com>
2026-07-11 19:03:59 +0530

    establish standardized API response and error handling

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,9 @@
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch((error) => next(error));
-};
+const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
 
 
 export { asyncHandler }
+
