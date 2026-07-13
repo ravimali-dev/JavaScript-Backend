1e9d5ef8d04b2f0c2b9ce2d922756fc334604732
Ravi Mali <ravimalakar091@gmail.com>
2026-07-13 23:11:05 +0530

    add password hashing and verification to user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -47,10 +47,13 @@ const UserSchema = new Schema(
 );
 
 UserSchema.pre("save", async function(next){
-    this.password = await(bcrypt.hash(this.password));
+    if(!this.isModified("password")) return next();
+    this.password = await(bcrypt.hash(this.password, 10));
     next();
 })
 
-
+UserSchema.methods.isPasswordCorrect = async function(password) {
+    return await bcrypt.compare(password, this.password);
+}
 
 export const User = mongoose.model("User", UserSchema);
