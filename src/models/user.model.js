98aa3f97d4b1c30c330eb8498c57c56aaeb0e412
Ravi Mali <ravimalakar091@gmail.com>
2026-07-14 13:47:20 +0530

    feat: add JWT token generation methods and refresh token field to User model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const UserSchema = new Schema(
   {
@@ -40,6 +41,9 @@ const UserSchema = new Schema(
       type: String,
       required: [true, "Password is required"],
     },
+    refreshToken:{
+      type: String,
+    },
   },
   {
     timestamps: true,
@@ -48,12 +52,39 @@ const UserSchema = new Schema(
 
 UserSchema.pre("save", async function(next){
     if(!this.isModified("password")) return next();
-    this.password = await(bcrypt.hash(this.password, 10));
+    this.password = await bcrypt.hash(this.password, 10);
     next();
 })
 
 UserSchema.methods.isPasswordCorrect = async function(password) {
     return await bcrypt.compare(password, this.password);
+}
+
+UserSchema.methods.generateAccessToken = function(){
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: this.fullName
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+UserSchema.methods.generateRefreshToken = function(){
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
 }
 
 export const User = mongoose.model("User", UserSchema);
