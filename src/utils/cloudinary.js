64d319af71114c7f10dec3d0a7949e0f679a54a4
Ravi Mali <ravimalakar091@gmail.com>
2026-07-22 22:52:36 +0530

    setup cloudinary

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,3 +7,22 @@ import { v2 as cloudinary } from 'cloudinary';
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
+
+    const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null;
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto",
+        });
+
+        fs.unlinkSync(localFilePath);
+
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        return null;
+    }
+};
+
+export { uploadOnCloudinary };
