24a511fa8e1d733d12251b2479209fc21dbe7efd
Ravi Mali <ravimalakar091@gmail.com>
2026-07-24 20:41:03 +0530

    solov a bug in index.js

--- src/index.js
+++ src/index.js
@@ -1,55 +1,17 @@
 import connectDB from "./db/index.js";
-import { app } from "./app.js"
-// import express from "express"
-// import mongoose from "mongoose"
-// import { DB_NAME } from "./constants.js"
+import { app } from "./app.js";
 
 connectDB()
- .then(() => {
+  .then(() => {
     app.on("error", (error) => {
       console.log("Server error:", error);
       throw error;
     });
+
     app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+      console.log(`Server running on port ${process.env.PORT || 8000}`);
     });
   })
   .catch((err) => {
     console.log("MongoDB connection failed!", err);
   });
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const app = express();
-
-//  app.get('/',(req, res) => {
-//             res.send(`App is listening on port ${process.env.PORT}`)
-//         });
-
-// (async () =>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-//         app.on("error", (error) => {
-//             console.log("ERROR: ", error);
-//             throw  error
-//         })
-
-       
-
-//         app.listen(process.env.PORT, () => {
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
-//     } catch (error) {
-//         console.log("MONGODB connection FAILED:", error)
-//     }
-// })()
