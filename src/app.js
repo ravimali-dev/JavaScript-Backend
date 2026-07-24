eeae70115969c41a39ce613cdc9c8bf82f942066
Ravi Mali <ravimalakar091@gmail.com>
2026-07-24 15:31:19 +0530

    connect router in app.js

--- src/app.js
+++ src/app.js
@@ -9,10 +9,23 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({limit: "16kb"}));
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+
+
+
+
+
+
+
+// ===== ROUTES IMPORT ===== (ye yahan add karo)
+import userRouter from "./routes/user.routes.js"
+
+// ===== ROUTES DECLARATION =====
+app.use("/api/v1/users", userRouter)
 
 
 
